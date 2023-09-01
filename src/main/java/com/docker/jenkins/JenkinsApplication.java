2a3a68c9a2db2dcb3b38735ffcf5f91a7e3027b7
John Mwai <mwaijohng@gmail.com>
2023-09-01 12:54:13 +0300

    added testing endpoint

--- src/main/java/com/docker/jenkins/JenkinsApplication.java
+++ src/main/java/com/docker/jenkins/JenkinsApplication.java
@@ -2,10 +2,19 @@ package com.docker.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @SpringBootApplication
+@RestController
 public class JenkinsApplication {
 
+	@GetMapping("/")
+	public String test(){
+		return "WORKING: " + new Date();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsApplication.class, args);
 	}
